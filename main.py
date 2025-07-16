982a612c2cf59d2ddb3aeb134d1df6db307ce4bf
Pedro Guilherme <cubomagico135z@gmail.com>
2025-07-15 21:20:02 -0300

    fix: registra blueprint das rotas

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-#main.py
 from flask import Flask, send_from_directory
 from flask_cors import CORS
 from app.routes import routes
-import os
 
 app = Flask(__name__)
-CORS(app,
-     origins=["https://frontend-s52u.vercel.app"],
-     supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization"],
-     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+CORS(
+    app,
+    origins=["https://frontend-s52u.vercel.app"],
+    supports_credentials=True,
+    allow_headers=["Content-Type", "Authorization"],
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 )
+
+# REGISTRA SUAS ROTAS!
+app.register_blueprint(routes)
 
 # Rota para servir uploads:
 @app.route("/uploads/<path:filename>")
@@ -19,3 +21,4 @@ def servir_arquivo(filename):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
